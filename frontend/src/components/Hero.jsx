2682c14e00619043c872d9056c6b14b03995dcd0
Kenny Cen <kennycen1976@gmail.com>
2025-01-22 19:29:45 -0500

    added motion animation to components

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,33 +1,53 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { assets } from '../assets/assets'
+import { motion } from "motion/react"
 
 const Hero = () => {
   return (
     <div className="relative z-0 bg-white">
       <div className="flex flex-col-reverse max-w-7xl mx-auto py-10 px-4 sm:py-24 sm:px-6 lg:px-8 lg:flex-row">
           <div className="space-y-8 lg:mt-28">
-            <h1 className="font-prata text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
-              Share the Joy of Reading
-            </h1>
-            <p className="text-xl text-gray-500 max-w-3xl">
-            Donate your books and discover new stories. Join our community of book lovers, where every book finds a new home and every story inspires a new journey. Together, we can make a difference, one page, one story, and one reader at a time.
-            </p>
+            <motion.h1 
+              initial={{y: -30, opacity: 0}}
+              whileInView={{y: 0, opacity: 1}}
+              transition={{duration: 0.8, delay: 0.5}}
+              className="font-prata text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
+                Share the Joy of Reading
+            </motion.h1>
+            <motion.p 
+              initial={{opacity: 0}}
+              whileInView={{opacity: 1}}
+              transition={{duration: 0.6, delay: 0.7}}
+              className="text-xl text-gray-500 max-w-3xl">
+                Donate your books and discover new stories. Join our community of book lovers, where every book finds a new home and every story inspires a new journey. Together, we can make a difference, one page, one story, and one reader at a time.
+            </motion.p>
             <div className="flex space-x-4">
-              <Link 
+              <motion.Link 
+                initial={{y: 30, opacity: 0}}
+                whileInView={{y: 0, opacity: 1}}
+                transition={{duration: 0.6, delay: 1}}
                 to="/donate"
                 className="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 hover:drop-shadow-xl"
               >
                 Donate Now
-              </Link>
-              <Link to="/inventory" className="inline-flex items-center px-6 py-3 border border-gray-300 text-base font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 hover:drop-shadow-xl">
-                Browse Books
-              </Link>
+              </motion.Link>
+              <motion.Link 
+                initial={{y: 30, opacity: 0}}
+                whileInView={{y: 0, opacity: 1}}
+                transition={{duration: 0.6, delay: 1.2}}
+                to="/inventory" className="inline-flex items-center px-6 py-3 border border-gray-300 text-base font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 hover:drop-shadow-xl">
+                  Browse Books
+              </motion.Link>
             </div>
           </div>
-          <div className="relative h-64 sm:h-80 md:h-96 lg:h-[34rem]">
+          <motion.div 
+          initial={{scale: 0}}
+          whileInView={{scale: 1}}
+          transition={{duration: 0.8, type: 'spring', stiffness: 40}}
+          className="relative h-64 sm:h-80 md:h-96 lg:h-[34rem]">
             <img className="w-full h-full object-contain" src={assets.hero} alt="Books on a shelf"/>
-          </div>
+          </motion.div>
       </div>
     </div>
   )
--- frontend/src/components/Organizations.jsx
+++ frontend/src/components/Organizations.jsx
@@ -1,32 +1,45 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { motion } from "motion/react"
 
 const Organizations = () => {
   return (
     <div className='mb-14 w-full border-t-2 border-b-2'>
       <div className='max-w-7xl mx-auto py-8 px-4 sm:py-12'>
         <div className='flex gap-8 items-center justify-center'>
-          <div className='w-full max-w-[250px] flex justify-center'>
+          <motion.div 
+          initial={{opacity: 0, y: 20}}
+          whileInView={{opacity: 1, y: 0}}
+          transition={{duration: 0.5, delay: 0.5}}
+          className='w-full max-w-[250px] flex justify-center'>
             <img 
               className='h-20 sm:h-24 md:h-32 object-contain hover:scale-105 transition-transform duration-200' 
               src={assets.cuny} 
               alt="CUNY Logo" 
             />
-          </div>
-          <div className='w-full max-w-[250px] flex justify-center'>
+          </motion.div>
+          <motion.div 
+          initial={{opacity: 0, y: 20}}
+          whileInView={{opacity: 1, y: 0}}
+          transition={{duration: 0.5, delay: 0.7}}
+          className='w-full max-w-[250px] flex justify-center'>
             <img 
               className='h-20 sm:h-24 md:h-32 object-contain hover:scale-105 transition-transform duration-200' 
               src={assets.suny} 
               alt="SUNY Logo" 
             />
-          </div>
-          <div className='w-full max-w-[250px] flex justify-center'>
+          </motion.div>
+          <motion.div 
+          initial={{opacity: 0, y: 20}}
+          whileInView={{opacity: 1, y: 0}}
+          transition={{duration: 0.5, delay: 0.8}}
+          className='w-full max-w-[250px] flex justify-center'>
             <img 
               className='h-16 mr-5 sm:h-20 md:h-28 object-contain hover:scale-105 transition-transform duration-200' 
               src={assets.doe} 
               alt="DOE Logo" 
             />
-          </div>
+          </motion.div>
         </div>
       </div>
     </div>
